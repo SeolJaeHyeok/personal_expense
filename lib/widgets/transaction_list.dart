cf210783c20cd9c30d612c1a2c63431459892ca9
seoljaehyeok <milkboy2564@naver.com>
2021-01-12 00:30:42 +0900

    SingleChildScrollView 대신 ListView 사용

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,55 +12,53 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 350,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: [
-                  Container(
-                    child: Text(
-                      '\$${tx.amount}',
+      child: ListView(
+        children: transactions.map((tx) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.purple,
+                          width: 3,
+                          style: BorderStyle.solid)),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
                       style: TextStyle(
-                        color: Colors.purple,
-                        fontWeight: FontWeight.bold,
                         fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    margin: EdgeInsets.all(20),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.purple,
-                            width: 3,
-                            style: BorderStyle.solid)),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    Text(
+                      DateFormat().add_yMMMd().format(tx.date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
                       ),
-                      Text(
-                        DateFormat().add_yMMMd().format(tx.date),
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              margin: EdgeInsets.symmetric(vertical: 7, horizontal: 10),
-              elevation: 5,
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            margin: EdgeInsets.symmetric(vertical: 7, horizontal: 10),
+            elevation: 5,
+          );
+        }).toList(),
       ),
     );
   }
