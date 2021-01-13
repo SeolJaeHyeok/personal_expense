9e09fa18966602cb19b0bdead010f0d09118a45b
seoljaehyeok <milkboy2564@naver.com>
2021-01-13 14:50:15 +0900

    Chart Widget 작성했는데 IDE 오류 발생 ㅠㅠㅠㅠㅠㅠ

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expense_app/models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index),);
+
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == weekDay.day &&
+            recentTransaction[i].date.month == weekDay.month &&
+            recentTransaction[i].date.year == weekDay.year &&) {
+          totalSum += recentTransaction[i].amount;
+        }
+      }
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+    );
+  }
+}
