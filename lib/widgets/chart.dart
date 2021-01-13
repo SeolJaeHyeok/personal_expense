00973c58862c0a5e043ca812fccf46d98b7bf759
seoljaehyeok <milkboy2564@naver.com>
2021-01-13 15:15:26 +0900

    오타 수정

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_app/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -12,11 +13,11 @@ class Chart extends StatelessWidget {
 
       var totalSum = 0.0;
 
-      for (var i = 0; i < recentTransaction.length; i++) {
-        if (recentTransaction[i].date.day == weekDay.day &&
-            recentTransaction[i].date.month == weekDay.month &&
-            recentTransaction[i].date.year == weekDay.year &&) {
-          totalSum += recentTransaction[i].amount;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
         }
       }
       return {'day': DateFormat.E(weekDay), 'amount': totalSum};
